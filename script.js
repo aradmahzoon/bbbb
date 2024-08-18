b799d5eb8dbef1e0df334fcb08f434b96ca2531b
aradmahzoon <aradmahzoun@gmail.com>
2024-08-18 15:33:46 +0330

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("surpriseBtn").addEventListener("click", function() {
-    var message = document.getElementById("surpriseMessage");
-    message.classList.toggle("hidden");
-});
