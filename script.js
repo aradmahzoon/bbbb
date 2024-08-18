66333dae3a4723691d8f037d6603653c8b935c25
aradmahzoon <aradmahzoun@gmail.com>
2024-08-18 14:43:35 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("surpriseBtn").addEventListener("click", function() {
+    var message = document.getElementById("surpriseMessage");
+    message.classList.toggle("hidden");
+});
